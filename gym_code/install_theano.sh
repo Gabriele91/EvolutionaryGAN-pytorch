567aa10a22e4d42de8eeeed690df1c84fe4b3503
Gabriele <dbgabri@gmail.com>
2020-03-10 16:21:05 -0600

    Added install_theano.sh

---
+++ gym_code/install_theano.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 -m pip install pillow==6.0.0
+python3 -m pip install sklearn
+python3 -m pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+python3 -m pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
