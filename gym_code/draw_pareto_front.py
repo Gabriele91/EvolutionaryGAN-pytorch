acc974f21ce86bcb0edf3430360cd369cf65b18e
Gabriele91 <dbgabri@gmail.com>
2020-03-18 11:30:07 -0600

    Update protter

--- gym_code/draw_pareto_front.py
+++ gym_code/draw_pareto_front.py
@@ -1,30 +1,55 @@
 import sys
+import numpy as np
 import matplotlib.pyplot as plt 
 
 def readtsv(path):
+    metric = False
     points = []
     with open(path,"r") as tsv:
         for line in tsv.readlines():
             point = line.split("\t")
-            points.append((float(point[0]),float(point[1])))
+            if len(point) == 3:
+                metric = True
+            if metric:
+                points.append((float(point[0]),float(point[1]), float(point[2])))
+            else:
+                points.append((float(point[0]),float(point[1])))
     points.sort(key=lambda point: point[0]) #sort by x
-    vx = [x for x,y in points]
-    vy = [y for x,y in points]
-    return vx,vy
+    vx = [p[0] for p in points]
+    vy = [p[1] for p in points]
+    vz = None
+    if metric:
+        vz = [abs(p[2]) for p in points]
+    return vx,vy,vz
 
 def main(path,savepath,invy=False):
-    x,y = readtsv(path)
+    x,y,z = readtsv(path)
     if invy:
         y_ = [-v for v in y]
         y = y_
     if savepath == None:
         plt.plot(x,y)
         plt.show()
+        if z is not None:
+            fig = plt.figure()
+            z = np.array(z)
+            z = 1.-(np.array(z)-np.min(z)) / (np.max(z)-np.min(z)) 
+            plt.plot(x,y)
+            plt.scatter(x, y, c=z, s=(z*13.0) ** 2, marker='o')
+            plt.show()
     else:
         fig = plt.figure()
         plt.plot(x,y)
         fig.savefig(savepath)
-
+        if z is not None:
+            pathsplit = savepath.split('.')
+            pathfile, ext = "".join(pathsplit[:-1]), pathsplit[-1] 
+            fig = plt.figure()
+            z = np.array(z)
+            z = 1.-(np.array(z)-np.min(z)) / (np.max(z)-np.min(z)) 
+            plt.plot(x,y)
+            plt.scatter(x, y, c=z, s=(z*13.0) ** 2, marker='o')
+            fig.savefig(savepath+"_mmd."+ext)
 
 
 if __name__ == "__main__":
